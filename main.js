d68bf55ee1595c5a0ef87d28a60a75273d94c8af
justin-law <57424752+justin-law@users.noreply.github.com>
2021-05-20 19:09:21 -0400

    Add phrases functionality

--- main.js
+++ main.js
@@ -11,43 +11,20 @@ client.once("ready", ()=> {
     console.log("JustinBot is online");
 });
 
-let badwords = ["hotay", "schlick", "pogchamp", "schlik", "shlick", "helb"];
+let badwords = ["hotay", "schlick", "pogchamp", "schlik", "shlick", "helb", "chikn", "lettuce", "terry"];
 
 //commands
 client.on("message", message => {
     if (message.author.bot) return;
-    /*
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    
-
-    if(command === "ping") {
-        message.channel.send("pong!");
-    } else if (command == "schlick") {
-        message.channel.send("stfu");
-    }
-    
-   const text = message.content.toLowerCase();
-   badwords.forEach(item => {
-    if (text.includes(item)) {
-        message.channel.send("Claire please shut the up");
-        return;
-   }
-   });  */
 
 
    if (message.content.startsWith(prefix)){
     const args = message.content.slice(prefix.length).split(" ");
-    //console.log("args:");
-    //console.log(args);
 
-    command = "";
+    var command = "";
 
-    if(!(typeof args[1] === 'undefined')) {
-        command = args[1].toLowerCase();
-    }
+    command = message.content.split(' ').slice(1).join(' ').toLowerCase();
 
-    //console.log(command);
     if (args[0] == "help"){
         message.channel.send(prefix+"add <word> \n"+prefix+"remove <word> \n"+prefix+"show");
     } else if (args[0] == "add") {
